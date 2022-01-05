63aa56d276b2cb1ecc45d1bb86bea3374e8f906d
Vipul <pawanvishwakarma521@gmail.com>
2022-01-05 23:15:19 +0530

    75, 179

---
+++ 179.largest-number.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=179 lang=java
+ *
+ * [179] Largest Number
+ */
+
+// @lc code=start
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] array = Arrays.stream(nums).mapToObj(String::valueOf).toArray(String[]::new);
+        Arrays.sort(array, (String s1, String s2) -> (s2 + s1).compareTo(s1 + s2));
+        return Arrays.stream(array).reduce((x, y) -> x.equals("0") ? y : x + y).get();
+    }
+}
+// @lc code=end
---
+++ 75.sort-colors.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=75 lang=java
+ *
+ * [75] Sort Colors
+ */
+
+// @lc code=start
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+    }
+}
+// @lc code=end
