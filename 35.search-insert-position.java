58fc5de6e1bcd0804d1fe3b6119722fa0e156d6c
Vipul <pawanvishwakarma521@gmail.com>
2022-01-09 23:06:00 +0530

    35

---
+++ 35.search-insert-position.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=35 lang=java
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] A, int target) {
+        int low = 0, high = A.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (A[mid] == target)
+                return mid;
+            else if (A[mid] > target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+}
+// @lc code=end
