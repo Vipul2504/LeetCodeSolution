0ddccb08fd0d5992b53f65c9b7594d58ffa283ad
Vipul <pawanvishwakarma521@gmail.com>
2022-01-09 23:13:23 +0530

    852

---
+++ 852.peak-index-in-a-mountain-array.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=852 lang=java
+ *
+ * [852] Peak Index in a Mountain Array
+ */
+
+// @lc code=start
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int lo = 0, hi = A.length - 1;
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (A[mi] < A[mi + 1])
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+        return lo;
+    }
+}
+// @lc code=end
