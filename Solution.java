4fb9756e51df036f7a1e487a4bbde4a11602b58c
Vipul <pawanvishwakarma521@gmail.com>
2022-01-18 22:25:03 +0530

    67,190

---
+++ Solution.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=190 lang=java
+ *
+ * [190] Reverse Bits
+ */
+
+// @lc code=start
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result += n & 1;
+            n >>>= 1; // CATCH: must do unsigned shift
+            if (i < 31) // CATCH: for last digit, don't shift!
+                result <<= 1;
+        }
+        return result;
+    }
+}
+// @lc code=end
