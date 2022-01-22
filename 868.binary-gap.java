d7fbd363aac6bb70924a3a9bcc4fe1becd0bf03e
Vipul <pawanvishwakarma521@gmail.com>
2022-01-22 22:22:50 +0530

    868,1342

---
+++ 868.binary-gap.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=868 lang=java
+ *
+ * [868] Binary Gap
+ */
+
+// @lc code=start
+class Solution {
+    public int binaryGap(int N) {
+        int[] A = new int[32];
+        int t = 0;
+        for (int i = 0; i < 32; ++i)
+            if (((N >> i) & 1) != 0)
+                A[t++] = i;
+
+        int ans = 0;
+        for (int i = 0; i < t - 1; ++i)
+            ans = Math.max(ans, A[i + 1] - A[i]);
+        return ans;
+    }
+}
+// @lc code=end
