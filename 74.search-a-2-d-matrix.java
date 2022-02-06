a76b605ebe2276e1a39e35986bf097e36564506e
Vipul <pawanvishwakarma521@gmail.com>
2022-02-06 22:10:43 +0530

    74, 240

---
+++ 74.search-a-2-d-matrix.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=74 lang=java
+ *
+ * [74] Search a 2D Matrix
+ */
+
+// @lc code=start
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+
+        }
+        return false;
+    }
+}
+// @lc code=end
