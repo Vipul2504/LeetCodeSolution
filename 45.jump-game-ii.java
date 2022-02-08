67d0a1b11b5d90e4dfecf81631a1046ad3172bce
Vipul <pawanvishwakarma521@gmail.com>
2022-02-08 22:48:19 +0530

    45

---
+++ 45.jump-game-ii.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=45 lang=java
+ *
+ * [45] Jump Game II
+ */
+
+// @lc code=start
+class Solution {
+    public int jump(int[] A) {
+        int jumps = 0, curEnd = 0, curFarthest = 0;
+        for (int i = 0; i < A.length - 1; i++) {
+            curFarthest = Math.max(curFarthest, i + A[i]);
+            if (i == curEnd) {
+                jumps++;
+                curEnd = curFarthest;
+            }
+        }
+        return jumps;
+    }
+}
+// @lc code=end
