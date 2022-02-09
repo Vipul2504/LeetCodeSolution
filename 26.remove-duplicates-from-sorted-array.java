badf398f859b7ee5bc22996ac357c1217fd325f9
Vipul <pawanvishwakarma521@gmail.com>
2022-02-09 22:52:17 +0530

    26

---
+++ 26.remove-duplicates-from-sorted-array.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=26 lang=java
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int dupes = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                dupes++;
+            }
+            nums[i - dupes] = nums[i];
+        }
+        return nums.length - dupes;
+    }
+}
+// @lc code=end
