3d934c537151594b5b941e2527dd7c1a56ec779a
Vipul <pawanvishwakarma521@gmail.com>
2022-02-19 23:32:28 +0530

    1886

---
+++ 1886.determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=1886 lang=java
+ *
+ * [1886] Determine Whether Matrix Can Be Obtained By Rotation
+ */
+
+// @lc code=start
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        int n = mat.length;
+        int[] res[] = new int[n][n];
+        for (int i = 0; i < n; i++) { // clockwise 90
+            for (int j = 0; j < n; j++) {
+                res[i][j] = mat[n - 1 - j][i];
+            }
+        }
+
+        int[] res2[] = new int[n][n];
+        for (int i = 0; i < n; i++) { // clockwise 180
+            for (int j = 0; j < n; j++) {
+                res2[i][j] = res[n - 1 - j][i];
+            }
+        }
+
+        int[] res3[] = new int[n][n];
+        for (int i = 0; i < n; i++) { // clockwise 270
+            for (int j = 0; j < n; j++) {
+                res3[i][j] = res2[n - 1 - j][i];
+            }
+        }
+
+        // compare to 90,180,270 and itself
+        if (Arrays.deepEquals(target, res) || Arrays.deepEquals(target, res2) || Arrays.deepEquals(target, res3)
+                || Arrays.deepEquals(target, mat)) {
+            return true;
+        }
+        return false;
+    }
+}
+// @lc code=end
