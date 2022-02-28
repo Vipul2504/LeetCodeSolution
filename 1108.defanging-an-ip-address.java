bd0a8bf676abf6c1ef4ac690ffbbeda0b72520b1
Vipul <pawanvishwakarma521@gmail.com>
2022-02-28 23:26:01 +0530

    1108

---
+++ 1108.defanging-an-ip-address.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=1108 lang=java
+ *
+ * [1108] Defanging an IP Address
+ */
+
+// @lc code=start
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : address.toCharArray()) {
+            sb.append(c == '.' ? "[.]" : c);
+        }
+        return sb.toString();
+    }
+}
+// @lc code=end
