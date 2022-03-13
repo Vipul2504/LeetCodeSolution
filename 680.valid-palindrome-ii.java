f38f9eda2f69a3d81f6e0dd6c2b7e839d19b47b9
Vipul <pawanvishwakarma521@gmail.com>
2022-03-13 22:43:29 +0530

    680

---
+++ 680.valid-palindrome-ii.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=680 lang=java
+ *
+ * [680] Valid Palindrome II
+ */
+
+// @lc code=start
+class Solution {
+    public boolean validPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int head = 0, tail = s.length() - 1;
+        char cHead, cTail;
+        while (head <= tail) {
+            cHead = s.charAt(head);
+            cTail = s.charAt(tail);
+            if (!Character.isLetterOrDigit(cHead)) {
+                head++;
+            } else if (!Character.isLetterOrDigit(cTail)) {
+                tail--;
+            } else {
+                if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+                    return false;
+                }
+                head++;
+                tail--;
+            }
+        }
+
+        return true;
+    }
+}
+// @lc code=end
