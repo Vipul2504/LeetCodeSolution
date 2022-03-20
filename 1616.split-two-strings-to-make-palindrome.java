3c59fa9d66127c552f2e3a2a005bf80171328a5f
Vipul <pawanvishwakarma521@gmail.com>
2022-03-20 23:29:28 +0530

    1616

---
+++ 1616.split-two-strings-to-make-palindrome.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=1616 lang=java
+ *
+ * [1616] Split Two Strings to Make Palindrome
+ */
+
+// @lc code=start
+class Solution {
+    boolean isPalindrome(String s, int i, int j) {
+        while (i < j && s.charAt(i) == s.charAt(j)) {
+            ++i;
+            --j;
+        }
+        return i >= j;
+    }
+
+    boolean check(String a, String b) {
+        int i = 0, j = a.length() - 1;
+        while (i < j && a.charAt(i) == b.charAt(j)) {
+            ++i;
+            --j;
+        }
+        return isPalindrome(a, i, j) || isPalindrome(b, i, j);
+    }
+
+    public boolean checkPalindromeFormation(String a, String b) {
+        return check(a, b) || check(b, a);
+    }
+}
+
+// @lc code=end
