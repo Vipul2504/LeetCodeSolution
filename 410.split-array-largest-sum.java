628a9b01e2f4c2393f21426f1c4d6235a84c0631
Vipul <pawanvishwakarma521@gmail.com>
2022-03-26 23:36:57 +0530

    410

---
+++ 410.split-array-largest-sum.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=410 lang=java
+ *
+ * [410] Split Array Largest Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        int max = 0;
+        long sum = 0;
+        for (int num : nums) {
+            max = Math.max(num, max);
+            sum += num;
+        }
+        if (m == 1)
+            return (int) sum;
+        // binary search
+        long l = max;
+        long r = sum;
+        while (l <= r) {
+            long mid = (l + r) / 2;
+            if (valid(mid, nums, m)) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return (int) l;
+    }
+
+    public boolean valid(long target, int[] nums, int m) {
+        int count = 1;
+        long total = 0;
+        for (int num : nums) {
+            total += num;
+            if (total > target) {
+                total = num;
+                count++;
+                if (count > m) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
