fa9971d97b594024c85d93c1e50b014ceee98a06
Vipul <pawanvishwakarma521@gmail.com>
2022-04-04 21:46:30 +0530

    704

---
+++ 704.binary-search.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=704 lang=java
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
